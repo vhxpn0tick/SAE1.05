774dca309011c388827417b557eb18c8c51eace2
vhxpn0tick <mathieu.bovy@etu.univ-st-etienne.fr>
2024-01-12 09:54:48 +0100

    MAJ

---
+++ TP/Exercice3.py
@@ -0,0 +1,55 @@
+import re
+from datetime import datetime
+
+def tradpseudocsv(fichier):
+    evenements = []
+    evenement = {}
+    with open(fichier, 'r') as file:
+        lines = file.readlines()
+        
+    for line in lines:
+        if line.startswith('BEGIN:VEVENT'):
+            evenement = {}
+            continue
+        if line.startswith('END:VEVENT'):
+            evenements.append(evenement)
+            continue
+        if ':' in line :
+            key, value = line.strip().split(':', 1)
+            evenement[key] = value
+
+    csv_data_list = []
+
+    for evenement in evenements:
+        dtstart = datetime.strptime(evenement['DTSTART'], '%Y%m%dT%H%M%SZ')
+        dtend = datetime.strptime(evenement['DTEND'], '%Y%m%dT%H%M%SZ')
+        duration = dtend - dtstart
+
+        ID = evenement['UID']
+        date = dtstart.strftime('%d-%m-%Y')
+        heure = dtstart.strftime('%H:%M')
+        duree = '{:02}:{:02}'.format(duration.seconds // 3600, (duration.seconds // 60) % 60)
+        intitule = evenement.get('SUMMARY', '')
+        salle = evenement.get('LOCATION', '')
+        description_raw = evenement.get('DESCRIPTION', '')
+        description_parts = re.split(r'\\n', description_raw)
+
+        groupe = description_parts[2] if len(description_parts) > 2 else ''
+        if description_parts[3].startswith('(') :
+            prof =  'Pas de professeur'
+        else :
+            prof = description_parts[3]
+        salles = salle.split(',') if salle != "vide" else ["vide"]
+        professeurs = prof.split(',') if prof != "vide" else ["vide"]
+        if intitule.startswith('R1.07')  :
+            if groupe=="RT1-TP B2" or groupe =="RT1-TD B" or groupe=="RT1-S1" : 
+                csv_data = f'"UID = {ID}";\n"Date = {date}"; "Heure = {heure}"; "Durée = {duree}";\n"Ressource = {intitule}";\n"Salle = {", ".join(salles)}";\n"Professeur = {", ".join(professeurs)}";\n"Groupe = {groupe}";'
+                csv_data_list.append(csv_data)
+
+    return '\n\n'.join(csv_data_list)
+
+
+# Exemple d'utilisation
+fichier = input("Donnez le nom du fichier :\n")
+pseudocsv = tradpseudocsv(fichier)
+print(pseudocsv)
---
+++ TP/Exercice4.py
@@ -0,0 +1,71 @@
+import re
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+def tradpseudocsv(fichier):
+    evenements = []
+    evenement = {}
+    with open(fichier, 'r') as file:
+        lines = file.readlines()
+        
+    for line in lines:
+        if line.startswith('BEGIN:VEVENT'):
+            evenement = {}
+            continue
+        if line.startswith('END:VEVENT'):
+            evenements.append(evenement)
+            continue
+        if ':' in line :
+            key, value = line.strip().split(':', 1)
+            evenement[key] = value
+
+    csv_data_list = []
+    sept=oct=nov=dec=0
+
+    for evenement in evenements:
+        dtstart = datetime.strptime(evenement['DTSTART'], '%Y%m%dT%H%M%SZ')
+        dtend = datetime.strptime(evenement['DTEND'], '%Y%m%dT%H%M%SZ')
+        duration = dtend - dtstart
+
+        ID = evenement['UID']
+        date = dtstart.strftime('%d-%m-%Y')
+        heure = dtstart.strftime('%H:%M')
+        duree = '{:02}:{:02}'.format(duration.seconds // 3600, (duration.seconds // 60) % 60)
+        intitule = evenement.get('SUMMARY', '')
+        salle = evenement.get('LOCATION', '')
+        description_raw = evenement.get('DESCRIPTION', '')
+        description_parts = re.split(r'\\n', description_raw)
+
+        groupe = description_parts[2] if len(description_parts) > 2 else ''
+        if description_parts[3].startswith('(') :
+            prof =  'Pas de professeur'
+        else :
+            prof = description_parts[3]
+        salles = salle.split(',') if salle != "vide" else ["vide"]
+        professeurs = prof.split(',') if prof != "vide" else ["vide"]
+        if dtstart.strftime('%m')== '09' or dtstart.strftime('%m')=='10' or dtstart.strftime('%m')=='11' or dtstart.strftime('%m')=='12'  :
+            if groupe=="RT1-TP B2"  : 
+                csv_data = f'"UID = {ID}";\n"Date = {date}"; "Heure = {heure}"; "Durée = {duree}";\n"Ressource = {intitule}";\n"Salle = {", ".join(salles)}";\n"Professeur = {", ".join(professeurs)}";\n"Groupe = {groupe}";'
+                csv_data_list.append(csv_data)
+                if dtstart.strftime('%m')== '09' : 
+                    sept += 1
+                elif dtstart.strftime('%m')== '10' : 
+                    oct += 1
+                elif dtstart.strftime('%m')== '11' : 
+                    nov += 1
+                elif dtstart.strftime('%m')== '12' : 
+                    dec += 1
+
+
+
+    return '\n\n'.join(csv_data_list) , sept, oct , nov , dec 
+
+
+
+
+# Exemple d'utilisation
+fichier = input("Donnez le nom du fichier :\n")
+pseudocsv, sept ,oct ,nov ,dec= tradpseudocsv(fichier)
+print (sept ,",",oct,",",nov,",",dec)
+print(pseudocsv)
+print(sept,",",oct,",",nov,",",dec)
